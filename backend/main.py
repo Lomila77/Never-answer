731a61515e31441c64ad27b0f46de3930c7b4ed8
gcolomer <gcolomer@protonmail.com>
2025-07-02 11:52:29 +0200

    add: websocket protocol between client and ollama API

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 import logging
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 import uvicorn
+from backend.app.weboscket import stream_ollama_response
+
 app = FastAPI()
 
 
@@ -11,6 +13,17 @@ def main() -> dict[str, str]:
     except Exception as e:
         logging.error(f"Error: {e}")
         return {"error": str(e)}
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        async for chunk in stream_ollama_response(data):
+            await websocket.send_text(chunk["response"])
+            if chunk["done"]:
+                break
 
 
 if __name__ == "__main__":
