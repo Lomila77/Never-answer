e006aae866a64ce7cf9833b4b054a8a3fabcb9a8
LeinaCorporan1 <leina.corporan123@gmail.com>
2025-07-05 20:42:29 +0200

    feat: selection page (not finished)

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,9 +2,12 @@ import Plank from '../components/Plank';
 import Title from "../components/Title"
 import Form from '../components/Form';
 import { useState, useEffect } from 'react';
+import  Selection  from '../components/Selection';
 
 function Home({route}) {
     const [title, setTitle] = useState("Chat");
+    const [selection, setSelection] = useState(null);
+    const [isSelectionComplete, setIsSelectionComplete] = useState(false);
 
     useEffect(() => {
         setTitle(
@@ -16,13 +19,22 @@ function Home({route}) {
         );
     }, [route]);
 
-    // return <Plank componentChildren={
-        return (
+    const handleSubmitSelection = (selection) => {
+        setSelection(selection);
+        setIsSelectionComplete(true);
+        console.log("Selection submitted:", selection);
+    }
+
+    return (
+    !isSelectionComplete ? (
+        <Selection handlesubmitSelection={handleSubmitSelection} />
+    ) : (
         <div className='h-screen flex w-full flex-col'>
-             <h1 className='text-5xl text-center p-2 text-[var(--primary)]/70 drop-shadow-lg '>{title}</h1>
+            <h1 className='text-5xl text-center p-2 text-[var(--primary)]/70 drop-shadow-lg '>{title}</h1>
             <Form route={route} title={title} />
         </div>
-        )
+    )
+);
 }
 
 export default Home;
