af07b070e237ed32326f522eb6921d236f26806b
gcolomer <gcolomer@protonmail.com>
2025-07-06 16:34:14 +0200

    Amélioration des points de terminaison WebSocket pour gérer les entrées audio et textuelles. Ajout de la gestion des exceptions pour les déconnexions WebSocket et les entités non traitables. Suppression de l'ancien point de terminaison POST pour le chat vocal.

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 import logging
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 import uvicorn
 from backend.app.weboscket import Model
 import json
@@ -14,55 +14,77 @@ rag = RAG("/media/gcolomer/gcolomer/archive/enwiki20201020/")
 model_caller = Model()
 
 
-@app.post("/")
-def main() -> dict[str, str]:
-    try:
-        pass
-    except Exception as e:
-        logging.error(f"Error: {e}")
-        return {"error": str(e)}
-
-
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        user_query = await websocket.receive_text()
-        async for chunk in model_caller.stream_text_response(
-                prompt=PROMPT_TEMPLATE, user_query=user_query):
-            response_data = json.loads(chunk)
-            await websocket.send_text(response_data["response"])
+    try:
+        await websocket.accept()
+        while True:
+            data: str = await websocket.receive_text()
+            user_input: dict = json.load(data)
+            if "audio" in user_input:
+                audio_response = model_caller.groq_voice_chat(
+                    user_input["audio"], PROMPT_TEMPLATE)
+                await websocket.send_json({"audio": audio_response})
+            elif "text" in user_input:
+                async for chunk in model_caller.stream_text_response(
+                        PROMPT_TEMPLATE, user_input["text"]):
+                    text_response = json.loads(chunk)
+                    await websocket.send_json({"text": text_response})
+            else:
+                raise ValueError("Unproccessable entity")
+    except WebSocketDisconnect:
+        print("Websocket disconnected")
+        return {"success": "Webscocket closed"}
+    except ValueError as e:
+        print(e)
+        return {"error": e}
+    finally:
+        await websocket.close()
 
 
 @app.websocket("/ws/course")
 async def websocket_endpoint_course(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        user_query = await websocket.receive_text()
-        ressource = rag.similarity_search(user_query)
-        prompt = PROMPT_TEMPLATE_COURSE.format(rag_document=ressource)
-        async for chunk in model_caller.stream_text_response(
-                prompt=prompt, user_query=user_query):
-            response_data = json.loads(chunk)
-            await websocket.send_text(response_data["response"])
+    try:
+        await websocket.accept()
+        while True:
+            user_query = await websocket.receive_text()
+            ressource = rag.similarity_search(user_query)
+            prompt = PROMPT_TEMPLATE_COURSE.format(rag_document=ressource)
+            async for chunk in model_caller.stream_text_response(
+                    prompt=prompt, user_query=user_query):
+                response_data = json.loads(chunk)
+                await websocket.send_text(response_data["response"])
+    except WebSocketDisconnect:
+        print("Websocket disconnected")
+        return {"success": "Webscocket closed"}
+    except ValueError as e:
+        print(e)
+        return {"error": e}
+    finally:
+        await websocket.close()
 
 
 @app.websocket("/ws/evaluation")
 async def websocket_endpoint_evaluation(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        user_query = await websocket.receive_text()
-        ressource = rag.similarity_search(user_query)
-        prompt = PROMPT_TEMPLATE_EVALUATION.format(rag_document=ressource)
-        async for chunk in model_caller.stream_text_response(
-                prompt=prompt, user_query=user_query):
-            response_data = json.loads(chunk)
-            await websocket.send_text(response_data["response"])
+    try:
+        await websocket.accept()
+        while True:
+            user_query = await websocket.receive_text()
+            ressource = rag.similarity_search(user_query)
+            prompt = PROMPT_TEMPLATE_EVALUATION.format(rag_document=ressource)
+            async for chunk in model_caller.stream_text_response(
+                    prompt=prompt, user_query=user_query):
+                response_data = json.loads(chunk)
+                await websocket.send_text(response_data["response"])
+    except WebSocketDisconnect:
+        print("Websocket disconnected")
+        return {"success": "Webscocket closed"}
+    except ValueError as e:
+        print(e)
+        return {"error": e}
+    finally:
+        await websocket.close()
 
-
-@app.post("/speech")
-def voice_chat(audio: bytes):
-    return model_caller.groq_voice_chat(audio=audio, prompt=PROMPT_TEMPLATE)
 
 
 if __name__ == "__main__":
